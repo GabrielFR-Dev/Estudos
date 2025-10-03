68049b9f2b918213250ceb5b5033e51a58af1524
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-03 18:43:32 -0300

    evento useState e onClick feito

--- StateHook-onClick/ex01/src/app/page.js
+++ StateHook-onClick/ex01/src/app/page.js
@@ -1,9 +1,16 @@
-import styles from "./page.module.css";
+'use client';
+import { useState } from "react";
 
 export default function Home() {
+  const [mensagem, setMensagem] = useState('O dia está lindo !! clique no botão abaixo para ver uma mensagem surpresa');
+  
+  function alterarMensagem(){
+    setMensagem('Que você nunca deixe de acreditar em seus sonhos !!, sempre busque a felicidade !!');  
+  }
   return (
     <div>
-      
+      <p>{mensagem}</p>
+      <button onClick={ alterarMensagem }>Clique aqui !!</button>
     </div>
   );
 }
