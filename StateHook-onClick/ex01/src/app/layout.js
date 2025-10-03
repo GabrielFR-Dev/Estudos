edd5efea662c16824680d9bf5c9a05affe8ee0c0
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-03 18:28:46 -0300

    Instalei o react no primeiro exemplo
    criei o primeiro exemplo sobre o State Hook e onClick e configurei o React deletando códigos desnecessários

---
+++ StateHook-onClick/ex01/src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Exemplo State Hook onClick 01",
+  description: "Exemplo de uso do State Hook com evento onClick em React",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
