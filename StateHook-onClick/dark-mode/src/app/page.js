dd7ab363a71bb86ee1a38e23ed5d35a209625ccb
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-07 12:44:38 -0300

    efeito dark finalizado

--- StateHook-onClick/dark-mode/src/app/page.js
+++ StateHook-onClick/dark-mode/src/app/page.js
@@ -1,11 +1,32 @@
 'use client';
-import { usestate } from 'react';
-import styles from "./page.module.css";
+import estilos from "./page.module.css";
+import { useState } from "react";
+import { BsSun, BsMoon } from "react-icons/bs";
 
 export default function Home() {
+  const [alterarTema, setAlterarTema] = useState(false);
+
+  function trocarTemaPagina() {
+    setAlterarTema(!alterarTema);
+  }
+
   return (
-   <main>
-    
-   </main>
-  );
+    <div className={alterarTema ? estilos.dark_mode : estilos.light_mode}>
+      <header className={estilos.header_container}>
+        <h1>Coffe Shop</h1>
+        <button onClick={trocarTemaPagina}>{alterarTema ? <BsSun /> : <BsMoon />}</button>
+      </header>
+      <main className={estilos.main_container}>
+        <div>
+          <h2>A melhor cafeteria do Brasil</h2>
+          <p>
+            Venha conhecer a melhor cafeteria do Brasil! Localizada no coração de São paulo, oferemos cafés artesanais de alta qualidade e um ambiente acolhedor.
+            Desfute de nossas bebidas exclusivas e deliciosos acompanhamento enquanto relaxa em um espaço único. Esperamos por você para uma experiência inesquecível!
+          </p>
+          <button>Nossas Lojas</button>
+        </div>
+
+      </main>
+    </div>
+  )
 }
