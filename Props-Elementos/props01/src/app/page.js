c951efc469da9fc082648fa44c2ee3f407733945
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-09 19:56:07 -0300

    Estruturando as paginas e o codigo

--- Props-Elementos/props01/src/app/page.js
+++ Props-Elementos/props01/src/app/page.js
@@ -1,3 +1,5 @@
+/*Estrututando pagina teste Props */
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
---
+++ Props-Elementos/props01/src/componente/card/index.jsx
@@ -0,0 +1,11 @@
+/*Estruturando página teste */
+
+
+export default function (props) {
+    return (
+        <div>
+        <h1>{props.titulo}</h1>
+        <p>{props.conteudo}</p>
+        </div>
+    );
+}
