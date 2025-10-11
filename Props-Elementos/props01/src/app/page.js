414e665f8c8beb91ff84d1af6a1ab0776795a929
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-11 18:31:16 -0300

    Adicionando condicionais para cada elemento

--- Props-Elementos/props01/src/app/page.js
+++ Props-Elementos/props01/src/app/page.js
@@ -24,72 +24,84 @@ export default function Home() {
       <main className={estilos.secao_card}>
         <section className={estilos.container_card}>
           <Card
+            elemento={'ar'}
             signo={"Aquário"}
             dataInicio={"21/01"}
             dataFim={"19/02"}
             imagem={Aquario}
           />
           <Card
+            elemento={'agua'}
             signo={"Peixes"}
             dataInicio={"20/02"}
             dataFim={"20/03"}
             imagem={Peixes}
           />
           <Card
+            elemento={'fogo'}
             signo={"Áries"}
             dataInicio={"21/03"}
             dataFim={"20/04"}
             imagem={Aries}
           />
           <Card
+            elemento={'terra'}
             signo={"Touro"}
             dataInicio={"21/04"}
             dataFim={"21/05"}
             imagem={Touro}
           />
           <Card
+            elemento={'ar'}
             signo={"Gêmeos"}
             dataInicio={"22/05"}
             dataFim={"21/06"}
             imagem={Gemeos}
           />
           <Card
+            elemento={'agua'}
             signo={"Câncer"}
             dataInicio={"21/06"}
             dataFim={"23/07"}
             imagem={Cancer}
           />
           <Card
+            elemento={'fogo'}
             signo={"Leão"}
             dataInicio={"24/07"}
             dataFim={"23/08"}
             imagem={Leao}
           />
           <Card
+            elemento={'terra'}
             signo={"Virgem"}
             dataInicio={"24/08"}
             dataFim={"23/09"}
             imagem={Virgem}
           />
           <Card
+            elemento={'ar'}
             signo={"Libra"}
             dataInicio={"24/09"}
             dataFim={"23/10"}
             imagem={Libra}
           />
           <Card
+            elemento={'agua'}
             signo={"Escorpião"}
             dataInicio={"24/10"}
             dataFim={"22/11"}
             imagem={Escorpiao}
           />
           <Card
+            elemento={'fogo'}
             signo={"Sagitário"}
             dataInicio={"23/11"}
             dataFim={"21/12"}
             imagem={Sagitario}
           />
           <Card
+            elemento={'terra'}
             signo={"Capricórnio"}
             dataInicio={"22/12"}
             dataFim={"20/01"}
--- Props-Elementos/props01/src/componente/card/index.jsx
+++ Props-Elementos/props01/src/componente/card/index.jsx
@@ -1,13 +1,42 @@
 /*Estruturando página teste */
 import estilos from "./Card.module.css";
 import Image from "next/image";
+import { FaFire, FaWater } from 'react-icons/fa';
+import { MdAir } from 'react-icons/md';
+import { PiPlant } from 'react-icons/pi';
+
+
 
 export default function (props) {
-    return (
+  return (
+
+    <div className={props.elemento === "agua"
+      ? estilos.card_signo_agua
+      : props.elemento === "fogo"
+        ? estilos.card_signo_fogo
+        : props.elemento === "terra"
+          ? estilos.card_signo_terra
+          : props.elemento === "ar" && estilos.card_signo_ar
+    }>
+
       <div className={estilos.container_card}>
-        <h3>{props.signo}</h3>
-        <Image src={props.imagem} alt={props.signo} className={estilos.image}/>
+        <div className={estilos.container_titulo}>
+          <h3>{props.signo}</h3>
+          {/*CONDICIONAL PARA DEFINIR O ICONE NO CARD */}
+
+          {props.elemento === "agua" ? (
+            <FaWater className={estilos.icone} />
+          ) : props.elemento === "fogo" ? (
+            <FaFire className={estilos.icone} />
+          ) : props.elemento === "terra" ? (<PiPlant className={estilos.icone} />
+          ) : props.elemento === "ar" && (<MdAir className={estilos.icone} />
+          )}
+        </div>
+
+        <Image src={props.imagem} alt={props.signo} className={estilos.image} />
         <span>{props.dataInicio} - {props.dataFim}</span>
       </div>
-    );
+
+    </div>
+  );
 }
