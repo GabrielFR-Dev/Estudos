103496882effe388426e9cfaf3939d43b490bb41
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-12 17:26:58 -0300

    codando a estrutura e importando elementos nos componentes

---
+++ Props-Elementos/projeto-base-props/src/componente/CardDepoimento/index.jsx
@@ -0,0 +1,8 @@
+import estilos from "./CardDepoimento.module.css";
+
+
+export default function CardDepoimento (props) {
+    return(
+        <div></div>
+    )
+}
---
+++ Props-Elementos/projeto-base-props/src/componente/MolduraInformações/index.jsx
@@ -0,0 +1,8 @@
+import estilos from "./MolduraInformações.module.css";
+
+
+export default function MolduraInformações(props) {
+    return (
+        <div></div>
+    )
+}
