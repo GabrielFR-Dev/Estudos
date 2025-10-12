e3e16819fa316d8fdb79cd32b640f8b32f82fc0a
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-12 17:30:53 -0300

    Importando o comando para utilizar imagens e importando as imagens
    importando imagens que utilizarei no site

--- Props-Elementos/projeto-base-props/src/app/page.js
+++ Props-Elementos/projeto-base-props/src/app/page.js
@@ -1,4 +1,12 @@
 import estilos from "./page.module.css";
+import Image from "next/image";
+import Aparelho from "/public/aparelho.png";
+import Cliente01 from "/public/cliente01.png";
+import Cliente02 from "/public/cliente02.png";
+import Cliente03 from "/public/cliente03.png";
+import Dente from "/public/dente.png";
+import Dentista01 from "/public/dentista.png";
+import Dentista02 from "/public/dentista.png";
 
 export default function Home() {
   return (
