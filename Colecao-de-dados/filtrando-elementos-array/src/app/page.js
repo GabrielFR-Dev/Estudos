09012bd8cad356f7358a62449d46b8f6633b0b85
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-23 19:25:56 -0300

    criando botoes

--- Colecao-de-dados/filtrando-elementos-array/src/app/page.js
+++ Colecao-de-dados/filtrando-elementos-array/src/app/page.js
@@ -1,10 +1,45 @@
+'use client';
 import Image from "next/image";
-import styles from "./page.module.css";
+import estilos from "./page.module.css";
+import { useState } from "react";
+import { jogosExclusivos } from "@/dados/jogos";
+import ItemJogos from "./componentes/ItemJogos";
 
 export default function Home() {
+
+  const [listaJogos, setListaJogos] = useState(jogosExclusivos);
+  
+  const handleFiltrarJogoPlataforma = (plataforma) => {
+    setListaJogos(jogosExclusivos.filter((jogo) => jogo.plataforma === plataforma ))
+  }
+
+  const handleLimparFiltro = () => {
+    setListaJogos(jogosExclusivos)
+  }
+  
+  
   return (
-    <div>
-   
+    <div className="estilos.container_principal">
+      <h2>Lista de Jogos Exclusivos</h2>
+      
+      <div className={estilos.container_btns}>
+        <button onClick={() => handleFiltrarJogoPlataforma("xbox")}>XBOX</button>
+        <button onClick={() => handleFiltrarJogoPlataforma("playstation")}>PlayStation</button>
+        <button onClick={() => handleFiltrarJogoPlataforma("nintendo")}>Nintendo</button>
+        <button onClick={() => handleLimparFiltro()}>Limpar Filtro</button>
+      </div>
+
+      <div className={estilos.container_cards}>
+        { listaJogos.map((jogo, index) => (
+          <ItemJogos 
+            key={index}
+            nome={jogo.nome}
+            plataforma={jogo.plataforma}
+          />
+        ))}
+
+      </div>
+      
     </div>
   );
 }
--- Colecao-de-dados/filtrando-elementos-array/src/dados/jogos.js
+++ Colecao-de-dados/filtrando-elementos-array/src/dados/jogos.js
@@ -7,8 +7,8 @@ export const jogosExclusivos = [
     {id: 6, nome: "Marvel 's Spider-man 2", plataforma: "playstation"},
     {id: 7, nome: "Death Stranding 2", plataforma: "playstation"},
     {id: 8, nome: "Final Fantasy XVI", plataforma: "playstation"},
-    {id: 9, nome: "Mario Odissey", plataforma: "nitendo"},
-    {id: 10, nome: "Zelda: Tears of Kingdom", plataforma: "nitendo"},
-    {id: 11, nome: "Xenoblade Chronicles 3", plataforma: "nitendo"},
-    {id: 12, nome: "Pokemon Scarlet & Violet", plataforma: "nitendo"}
+    {id: 9, nome: "Mario Odissey", plataforma: "nintendo"},
+    {id: 10, nome: "Zelda: Tears of Kingdom", plataforma: "nintendo"},
+    {id: 11, nome: "Xenoblade Chronicles 3", plataforma: "nintendo"},
+    {id: 12, nome: "Pokemon Scarlet & Violet", plataforma: "nintendo"}
 ];
