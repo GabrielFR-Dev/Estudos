b541bfe70d2384ff87503b04d6aaf44e6a6d6a8f
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-23 19:37:23 -0300

    Importando imagens e criando o componente ItemJogos

---
+++ Colecao-de-dados/filtrando-elementos-array/src/app/componentes/ItemJogos/index.jsx
@@ -0,0 +1,26 @@
+import estilos from "./ItemJogos.module.css";
+import Image from "next/image";
+import xboxlogo from "/public/xbox-logo.png";
+import playstationLogo from "/public/playstation-logo.jpg";
+import nintendoLogo from "/public/nintendo-logo.jpg";
+
+export default function ItemJogos ({ nome, plataforma}) {
+    return (
+        <div className={estilos.card}>
+            <figure>
+                <Image
+                    src={
+                        plataforma === 'xbox'
+                        ? xboxlogo
+                        : plataforma === 'playstation'
+                        ? playstationLogo
+                        : nintendoLogo
+                    }
+                />
+            </figure>
+            <div>
+                <p>{nome}</p>
+            </div>
+        </div>
+    );
+}
