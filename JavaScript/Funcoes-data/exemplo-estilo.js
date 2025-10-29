f246f09d6a86618a786b854986891caabafe219e
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-28 22:43:22 -0300

    função data

---
+++ JavaScript/Funcoes-data/exemplo-estilo.js
@@ -0,0 +1,16 @@
+const dataAtual = new Date();
+
+const MesAno = {
+    month: "long",
+    year: 'numeric',
+}
+console.log( dataAtual.toLocaleString('en-US', MesAno) );
+
+const SemanaHora = {
+    weekday: "long",
+    hour: 'numeric',
+    minute: 'numeric',
+
+}
+
+console.log( dataAtual.toLocaleString('en-US', SemanaHora) );
---
+++ JavaScript/Funcoes-data/setters.js
@@ -0,0 +1,24 @@
+const dataAtual = new Date();
+
+dataAtual.setFullYear(2005);        // Altera o ano
+console.log( dataAtual );
+
+dataAtual.setMonth(1);              // Altera o mês (0 á 11)
+console.log( dataAtual );
+
+dataAtual.setDate(20);              // Altera o dia do mês
+console.log( dataAtual );
+
+dataAtual.setHours(11);             // Altera a hora (0 á 23)
+console.log( dataAtual );
+
+dataAtual.setMinutes(0);            // Altera os minutos (0 á 59)
+console.log( dataAtual );
+
+dataAtual.setSeconds(59);           // Altera os segundos (0 á 59)
+console.log( dataAtual );
+
+dataAtual.setMilliseconds(15);      // Altera os millisegundos
+console.log( dataAtual );
+
+
