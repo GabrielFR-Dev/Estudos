ca7306ad6faf3612c48d38caeae7d1ad30f0105f
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 20:46:20 -0300

    Estrutura de repeticao

--- JavaScript/Estrutura-repeticao/For/for_of/for_of_3.js
+++ JavaScript/Estrutura-repeticao/For/for_of/for_of_3.js
@@ -1,8 +1,10 @@
-let notas_trimestrais  = [9, 7, 6, 7];
+let notas_trimestrais  = [9, 7, 6, 7]; //Array com notas trimestrais
 
 let total_notas  = notas_trimestrais.length;
 let soma_notas  = 0;
 
+
+//Percorrendo o array de notas
 for ( let nota of notas_trimestrais ){
     
     soma_notas = soma_notas + nota;
@@ -11,6 +13,9 @@ for ( let nota of notas_trimestrais ){
 let media = soma_notas / total_notas;
 
 console.log("Média: " + media);
+
+
+//O status será impresso de acordo com a média
 
 if (media >= 7 ) {
     console.log("Aprovado");
