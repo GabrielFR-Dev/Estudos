6a95ffa3f7d8514c4b7d3baa1ccde1306a123c88
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 07:16:47 -0300

    Alterando elemento array

---
+++ JavaScript/Colecao-dados-Array/alterando-elementos-array/index.js
@@ -0,0 +1,16 @@
+// Para alterar um elementoprecisamos do seu índice (posição) dentro do array
+
+
+var disciplinas = [
+    "Português",         // Indice 0
+    "Matemática",        // Indice 1
+    "História",          // Indice 2
+    "Geografia",         // Indice 3
+    "Química",           // Indice 4
+    "Física",            // Indice 5
+    "Inglês"             // Indice 6   Elemento que vai ser alterado
+];
+
+disciplinas[6] = "Francês";  //Trocando o valor do elemento
+
+console.log(disciplinas) // Vai substituir o Ingles do array por Francês
