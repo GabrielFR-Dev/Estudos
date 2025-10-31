e758099778182f16df1450253cd60cb00129cc2f
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 07:10:11 -0300

    Acessando um array

---
+++ JavaScript/Colecao-dados-Array/acessando-elementos-array/index.js
@@ -0,0 +1,14 @@
+// Para acessar um elemento dentro de um array precisamos de um indice
+// Um índice representa a posição que um elemento ocupa dentro do Array
+// Cada elemento do array é acessado por um índice. Começando sempre do  [0]
+
+var cursos = [
+    "Introdução ao Dart",                           // índice 0
+    "Variáveis em PHP",                             // índice 1
+    "Tipos de dados em java",                       // índice 2
+    "Estruturas condicionais em Python",            // índice 3
+    "Funções em JavaScript"                         // índice 4
+];
+
+console.log(cursos[4]);    // Vai imprimir Funções em JavaScript
+console.log(cursos [0]);   // vai imprimir Introduçao ao Dart
