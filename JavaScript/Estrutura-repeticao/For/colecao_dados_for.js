74117e4d1d07d654fc94f5bbb456bd7ec339150c
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 20:23:57 -0300

    Estrutura de repeticao

--- JavaScript/Estrutura-repeticao/For/colecao_dados_for.js
+++ JavaScript/Estrutura-repeticao/For/colecao_dados_for.js
@@ -1,1 +1,9 @@
-// Podemos utilizar a
+// Podemos utilizar a estrutura de repetição for para percorrer coleções de dados, como array
+
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+for(contador = 0; contador < meses.length; contador++) {
+    console.log(meses(contador));
+}
+
+//Independentemente do tamnho que uma coleção de dados possa ter, podemos percorrê-la com a estrutura de repetição for
