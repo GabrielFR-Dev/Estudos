740fe0a4730e70b40e5ed2699a24154930706c10
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 19:33:52 -0300

    Estrutura de condição

---
+++ JavaScript/Estrutura-repeticao/While/while.js
@@ -0,0 +1,14 @@
+let contador = 0;
+let multiplicador = 6;
+
+console.log(`Tabuada do ${multiplicador } \n`);
+
+while(contador <= 10){ //Condição da estruta de repetição while
+    
+   
+   //O valor da multiplicação entre os números é guardado na variável resultado
+    let soma = contador * multiplicador;                           
+    console.log(`${multiplicador} X ${contador} = ${soma}`) // Impressão das variaveis
+    contador++ //Incrimento do contador                                         
+                                                    
+}
