ff947013076ef07115c098c63ccdaed6991b6fef
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 20:51:15 -0300

    Operadores

---
+++ JavaScript/Estruturas-condicionais/operadores.js
@@ -0,0 +1,29 @@
+// Operador da estrutura de condição
+
+
+//Operadores logiocos
+
+// ||  Ou
+
+// &&  E
+
+// !  Negação
+
+
+//Operadores relacionais
+
+// ==     Igual
+
+// !=     Diferente
+
+// ===    Igualdade estrita
+
+// !==    Desigualdade estrita
+
+// <      Menor
+
+// >      Maior
+
+// <=     Menor ou Igual
+
+// >=     Maior ou igual
