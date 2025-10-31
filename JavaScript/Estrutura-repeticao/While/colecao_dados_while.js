924a8a1e4580ca10efcc90ee76bc0c4d07c3d819
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 19:51:33 -0300

    Estrutura repeticao

---
+++ JavaScript/Estrutura-repeticao/While/colecao_dados_while.js
@@ -0,0 +1,10 @@
+let alunos = ["Gabriel", "Hemily", "Fernanda", "Pedro"]; //array de alunos
+
+let contador = 0;
+
+while( contador < alunos.length ) {
+    let aluno_nome = alunos[contador];
+
+    console.log("Aluno: " + aluno_nome);
+    contador++
+}
