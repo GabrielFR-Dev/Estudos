22b26e4ae946cbafca2928ec5c2ebb880c73fcac
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 20:34:30 -0300

    Coleção objetos

---
+++ JavaScript/Colecao-dados-Array/Colecao-objetos/index.js
@@ -0,0 +1,9 @@
+// quando temos objetos agrupador em uma mesma variavel, temos uma coleção de objetos
+
+let clientes_array = [
+    {id: 1, nome: "Mônica", telefone: "99999-9999"},
+    {id: 2, nome: "Gabriel", telefone: "99999-9999"},
+    {id: 3, nome: "Fernanda", telefone: "99999-9999"},
+];
+
+//Acima temos o array clientes_array. dentro dele temos objetos que representam clientes
