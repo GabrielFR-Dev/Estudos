273b8ae9f03e76a0a95f195b005e0c9133a81d7d
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 21:19:41 -0300

    Tratamento string

--- JavaScript/Manipulacao-strings/Substituindo-partes-string/substituindo_string.js
+++ JavaScript/Manipulacao-strings/Substituindo-partes-string/substituindo_string.js
@@ -1,3 +1,12 @@
+//Quando é necessário substituir uma substrinf, basta usar o método replace()
+
+//O método replace() substitui a primeira ocorrência encontrada
+
+//O metodo replace() procura a primeira vez que o termo DO MUNDO aparece no texto e substitui por DA AMERICA LATINA
+
+
+
+
 let texto = `O Rio de Janeiro é uma das cidades mais bonitas do mundo!
 Essa cidade possui muitos pontos turísticos e inúmeras opções de lazer.
 Quem vive nessa cidade é muito privilegiado!`;
