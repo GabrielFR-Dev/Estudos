894c6309f75edd1105b30b695a9d60772e55a9cf
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 21:22:46 -0300

    manipulação string

--- JavaScript/Manipulacao-strings/extraindo-partes-strings/retornando_string.js
+++ JavaScript/Manipulacao-strings/extraindo-partes-strings/retornando_string.js
@@ -1,3 +1,8 @@
+//O substring() é um método nativo do JavaScript que nos permite extrair partes de uma string
+
+//O conteudo extraido pelo substring é definido pela posição dos caracteres na string.... .substring(7, 18);
+
+
 let conteudo = "O brasil deveria ser pais de primeiro mundo, so que os politicos não são honestos";
 
 let tamanho = conteudo.length;
