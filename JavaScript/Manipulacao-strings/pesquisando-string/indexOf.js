539d234e4d904ef58c596d83b8284bad1f6d0a78
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 21:27:05 -0300

    Tratamento de string

--- JavaScript/Manipulacao-strings/pesquisando-string/indexOf.js
+++ JavaScript/Manipulacao-strings/pesquisando-string/indexOf.js
@@ -1,3 +1,7 @@
+//Para pesquisar por uma substrinf dentro de uma string basta usar o método indexOf("String que deseja preocurar")
+
+
+
 let expressao_proibida = "passaporte falso";
 
 let comentario = "     venda  PASSAPORTE FALSO "
@@ -19,3 +23,5 @@ if ( comentario.indexOf(expressao_proibida) > -1){
 // Utilizamos o .indexOf para pesquisar a expressao_proibida dentro da string.
 // Caso achamos a expressao proibida ele retornara a possição que ela está, ai sera exibida para enviar o comentario novamente.
 // Caso nao encontre ele retornara o valor -1, e com isso a o comentario sera aprovado !
+
+//Quando o método indexOf encontra a substring dentro de uma string, ele retorna a posição onde a substring foi encontrada. Caso não encontre ele retorna o valor -1
