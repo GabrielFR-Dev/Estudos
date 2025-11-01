69efa62a7f86aeb3d148acf0bf53f799a7a6d3d8
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-31 21:32:00 -0300

    tratando strings

---
+++ JavaScript/Manipulacao-strings/separando-string-partes/separando_string.js
@@ -0,0 +1,14 @@
+//Quando for necessário separar uma string em substrings, basta usar o metodo split()
+
+//O método split() separa a variável cores_string em substrings menores. A cada espaço em branco que essa string tiver, ele cria um novo elemento
+
+// Com o método split() Quebramos a strinf. com isso podemos acessar o primeiro elemento do array. para obter o nome da string
+
+
+let cores_string = "Vermelho Azul Preto Verde"
+
+let corres_array = cores_string.split(" "); //(Caractere usado para separar a string)
+
+console.log(corres_array)
+
+// Utilizamos o método split() para separar as strings em partes.
--- JavaScript/Manipulacao-strings/separando_string.js
+++
@@ -1,7 +0,0 @@
-let cores_string = "Vermelho Azul Preto Verde"
-
-let corres_array = cores_string.split(" ");
-
-console.log(corres_array)
-
-// Utilizamos o método split() para separar as strings em partes.
