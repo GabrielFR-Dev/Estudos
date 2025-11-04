02dd2f7a8b92fcb5901a1b3d12a2d750dc3caeb5
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-11-03 21:03:15 -0300

    Funções nativas array

---
+++ JavaScript/Funcoes-de-array/push.js
@@ -0,0 +1,12 @@
+// Inserir um elemento em um array é bem simples. Basta utilizar o método push
+
+const diasDaSemana = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+]
+
+diasDaSemana.push("Sábado"); //Inserindo um elemento no array
