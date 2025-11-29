25364e17fea86946683b430a00b7b0167293b68a
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-11-29 08:04:22 -0300

    fazendo a camada do padrao  rest

---
+++ Back-end/APIs-aulas/mensagem-motivacao-api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+let data = new Date();
+
+let mensagemDoDia = require('./mensagemDoDia');
+
+app.get('/', (req, res) => {
+    let dia = data.getDate();
+    let mensagemSelecionada = mensagemDoDia.retornaMensagemDoDia(dia);
+
+    res.json({mensagem: mensagemSelecionada});
+});
+
+app.listen(8080, () => {
+    console.log('Servidor node iniciadoem: ' + data);
+})
+
+
