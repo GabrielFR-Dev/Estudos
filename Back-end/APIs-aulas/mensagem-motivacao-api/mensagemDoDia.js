28cc73a8bbaed0df890054543b0d74e33ddf693f
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-12-21 10:15:40 -0300

    jh

--- Back-end/APIs-aulas/mensagem-motivacao-api/mensagemDoDia.js
+++ Back-end/APIs-aulas/mensagem-motivacao-api/mensagemDoDia.js
@@ -1,8 +1,7 @@
-const { LiaFileExportSolid } = require('react-icons/lia');
 let dados = require('./dados');
 
 function retornaMensagemDoDia(dia) {
     return dados.frases[dia - 1];
 }
 
-LiaFileExportSolid.retornaMensagemDoDia = retornaMensagemDoDia;
+exports.retornaMensagemDoDia = retornaMensagemDoDia;
