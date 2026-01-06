1703e4c35de686927e3c61f58b3244e190e07774
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-06 18:35:20 -0300

    calculando o imc

---
+++ Back-end/Recebendo-parametro-back-end/Tratando-parametros-api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+
+    let peso = req.query.peso;
+    let altura = req.query.altura;
+
+    let imc = peso / (altura * altura);
+
+    res.json({imc: imc});
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log("Servidor inciado em: " + data);
+});
