65a20d4d259e5d2a22630a393067e6e0db359632
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-07 20:01:12 -0300

    calcula o imc e retorna um status informando o imc do corpo

---
+++ Back-end/Recebendo-parametro-back-end/json-com-mais-de-uma-chave/calculadoresIMC.js
@@ -0,0 +1,26 @@
+function efetuarCalculoIMC(peso, altura) {
+    let imc = peso / (altura * altura);
+    return imc;
+}
+
+function retornaStatusIMC(imc) {
+    let status;
+
+    if(unc < 18.5) {
+        status = 'Abaixo do peso';
+    }
+    else if(imc >= 18.5 && imc <  24.9)
+    {
+        status = 'Peso Normal';
+    }
+    else if(imc >= 24.9 && imc < 30)
+    {
+        status = 'Acima do pesoo';
+    }
+    else {
+        status = 'Obeso';
+    }
+}
+
+exports.efetuarCalculoIMC = efetuarCalculoIMC;
+exports.retornaStatusIMC = retornaStatusIMC;
---
+++ Back-end/Recebendo-parametro-back-end/json-com-mais-de-uma-chave/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const calculadoraIMC = require('./calculadoraIMC');
+
+app.get('/' , (req, res) => {
+    let peso = req.query.peso;
+    let altura = req.query.altura;
+    
+    let imc = calculadoraIMC.efetuarCalculoImc(peso, altura);
+    let status = calculadoraIMC.retornaStatusIMC(imc);
+
+    let json = { imc: imc, status: status};
+    res.json(json);
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log('Servidor iniciado em ' + data.toLocaleString());
+});
