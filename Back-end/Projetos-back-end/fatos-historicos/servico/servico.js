2389a469a955e04e12676e728b676dfe741fa24d
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-09 17:24:37 -0300

    validando numeros

--- Back-end/Projetos-back-end/fatos-historicos/servico/servico.js
+++ Back-end/Projetos-back-end/fatos-historicos/servico/servico.js
@@ -1,6 +1,32 @@
-function servicoBuscarFatoPorAno(ano)
-{
-    return ano;
+import fatosHistoricos from '../dados/fatos.js';
+
+
+function servicoValidaAno(ano) {
+
+    if(isNaN(ano))
+    {
+        return false
+    }
+    else
+    {
+        if(ano >= 1920 && ano <= 2020)
+        {
+            return true
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
 
-export default servicoBuscarFatoPorAno;
+function servicoBuscarFatoPorAno(ano) {
+    let fatoSelecionado = fatosHistoricos.find(fato => {
+        return fato.Ano == ano;
+    });
+
+    return fatoSelecionado.Fato;
+}
+
+export { servicoBuscarFatoPorAno, servicoValidaAno }
