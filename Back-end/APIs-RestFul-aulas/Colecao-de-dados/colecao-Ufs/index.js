22b1bba1574c1ffe47c7fc9104fac31eb83a9f31
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-13 18:35:29 -0300

    fazendo a segunda rota na api

--- Back-end/APIs-RestFul-aulas/Colecao-de-dados/colecao-Ufs/index.js
+++ Back-end/APIs-RestFul-aulas/Colecao-de-dados/colecao-Ufs/index.js
@@ -7,6 +7,13 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUF);
 });
 
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    const uf = colecaoUF.find(u => u.id === idUF);
+
+    res.json(uf);
+});
+
 app.listen(8080, () => {
     console.log('Servidor iniciado na porta 8080');
 });
