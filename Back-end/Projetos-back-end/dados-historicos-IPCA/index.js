90c43ee19ef251514bbcebbee5541edcca7e10bb
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-14 18:12:40 -0300

    criando as rotas

--- Back-end/Projetos-back-end/dados-historicos-IPCA/index.js
+++ Back-end/Projetos-back-end/dados-historicos-IPCA/index.js
@@ -2,9 +2,26 @@ import express from 'express';
 
 const app = express();
 
-app.get('/dadados', (req, res) => {
+
+app.get('/historico/ipca', (req, res) => {
+    
+});
+
+
+app.get('/historico/ipca/:ano', (req, res) => {
+
+    
+});
+
+
+app.get('/historico/ipca/:id', (req, res) => {
 
 });
+
+
+/* app.get('/historico/ipca/:calculo', (req, res) => {
+
+}); 4 rota de calculo */
 
 app.listen(8080, () => {
     console.log('Servidor rodando na porta 8080');
